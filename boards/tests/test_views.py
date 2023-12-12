210b1be0d7f1b070625e67ee15d1e18b1bc54db6
vanJker <1847722163@qq.com>
2023-12-12 20:27:39 +0800

    Login

--- boards/tests/test_views.py
+++ boards/tests/test_views.py
@@ -1,8 +1,8 @@
 from django.test import TestCase
 from django.urls import reverse, resolve
 from django.contrib.auth.models import User
-from boards.forms import NewTopicForm
 
+from boards.forms import NewTopicForm
 from boards.models import Board, Topic, Post
 from boards.views import board_topics, home, new_topic
 
