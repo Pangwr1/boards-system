00a1d98010458abd6b57a009662656346575c715
vanJker <1847722163@qq.com>
2023-12-12 19:46:27 +0800

    Log Out

--- boardsys/urls.py
+++ boardsys/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, re_path
+from django.contrib.auth import views as auth_views
 
 from accounts import views as accounts_views
 from boards import views
@@ -26,6 +27,7 @@ urlpatterns = [
 
     re_path(r'^$', views.home, name='home'),
     re_path(r'^signup/$', accounts_views.signup, name='signup'),
+    re_path(r'^logout/$', auth_views.LogoutView.as_view(), name='logout'),
     re_path(r'^boards/(?P<pk>\d+)/$', views.board_topics, name='board_topics'),
     re_path(r'^boards/(?P<pk>\d+)/new/$', views.new_topic, name='new_topic'),
     re_path(r'^admin/', admin.site.urls)
