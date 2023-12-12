280bb8bc91382b85416211d6dcc96c6327a8d672
vanJker <1847722163@qq.com>
2023-12-12 20:48:07 +0800

    Fix Labels

--- boards/forms.py
+++ boards/forms.py
@@ -7,9 +7,12 @@ class NewTopicForm(forms.ModelForm):
             attrs={'rows': 5, 'placeholder': '请简要介绍一下素材主题'}
         ), 
         max_length=4000,
-        help_text='最大长度为 4000 字'
+        help_text='最大长度为 4000 字',
+        label='简介'
     )
 
     class Meta:
         model = Topic
         fields = ['subject', 'message']
+
+        labels = {'subject': '主题'}
