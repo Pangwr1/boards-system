606335125176c92d69d0a1fc460fb0a0907b5628
vanJker <1847722163@qq.com>
2023-12-12 12:03:59 +0800

    URLs

--- boards/tests.py
+++ boards/tests.py
@@ -5,17 +5,24 @@ from boards.views import board_topics, home
 
 # Create your tests here.
 class HomeTests(TestCase):
-    def test_home_status_code(self):
+    def setUp(self) -> None:
+        self.board = Board.objects.create(name='好词', description='这是关于好词的素材版')
         url = reverse('home')
-        response = self.client.get(url)
-        self.assertEquals(response.status_code, 200)
+        self.response = self.client.get(url)
+
+    def test_home_status_code(self):
+        self.assertEquals(self.response.status_code, 200)
     
     def test_home_url_resolves_home_view(self):
         view = resolve('/')
         self.assertEquals(view.func, home)
     
+    def test_home_view_contains_link_to_topics_page(self):
+        board_topics_url = reverse('board_topics', kwargs={'pk': self.board.pk})
+        self.assertContains(self.response, 'href="{0}"'.format(board_topics_url))
+    
 class BoardTopicsTests(TestCase):
-    def setUp(self):
+    def setUp(self) -> None:
         Board.objects.create(name='好词', description='这是关于好词的素材版')
     
     def test_board_topics_view_success_status_code(self):
@@ -31,3 +38,9 @@ class BoardTopicsTests(TestCase):
     def test_board_topics_url_resolves_board_topics_view(self):
         view = resolve('/boards/1/')
         self.assertEquals(view.func, board_topics)
+
+    def test_board_topics_view_contains_link_to_homepage(self):
+        board_topics_url = reverse('board_topics', kwargs={'pk': 1})
+        response = self.client.get(board_topics_url)
+        home_url = reverse('home')
+        self.assertContains(self.response, 'href="{0}"'.format(home_url))
